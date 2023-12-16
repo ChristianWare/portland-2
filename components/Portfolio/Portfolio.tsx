109367cd9f3f623f8e82e7fe0867c37a07fe819c
Chris Ware <c.p.ware@hotmail.com>
2023-12-16 07:50:56 -0700

    Github link added

--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -3,13 +3,12 @@ import LayoutWrapper from "../LayoutWrapper";
 import styles from "./Portfolio.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import Button from "../Button/Button";
 
 const Portfolio = () => {
   return (
     <section className={styles.container} id='projects'>
       <LayoutWrapper>
-        <h2 className={styles.heading}>My previous work</h2>
+        <h2 className={styles.heading}>Selected Projects</h2>
         <div className={styles.bottom}>
           {projects.map((x, index) => (
             <div key={index} className={styles.box}>
@@ -17,16 +16,21 @@ const Portfolio = () => {
                 <Image src={x.src} alt={x.title} fill className={styles.img} />
               </div>
               <h3 className={styles.title}>{x.title}</h3>
-              <div className={styles.btnLink}>
-                <Button text='Github' btnType='tertiary' href='/' />
-              </div>
               <p className={styles.descr}>{x.description}</p>
+
               <Link
                 href={x.href}
                 className={styles.leanrMoreBtn}
                 target='_blank'
               >
                 Live Site &gt;
+              </Link>
+              <Link
+                href={x.github}
+                className={styles.leanrMoreBtnii}
+                target='_blank'
+              >
+                Github &gt;
               </Link>
             </div>
           ))}
--- lib/data.ts
+++ lib/data.ts
@@ -20,6 +20,7 @@ export const projects = [
     description:
       "This project is a redesign of Taco Bell's website. The front end was built with next.js, and the CMS is Sanity.io. The styling was done with CSS Modules so that I can have the most flexibility for design. Each menu item has its slug page where more details are given on that particular item.",
     href: "/",
+    github: "https://github.com/ChristianWare/Tacobell-ii",
   },
   {
     src: Cwagency,
@@ -27,6 +28,7 @@ export const projects = [
     description:
       "Chris Ware Agency is a website where users can learn about the benefits of having a direct booking website over third-party listing platforms like Airbnb or VRBO. This front-end project with a markdown blog utilizes the local file system with node.js. All styling was done with CSS Modules.",
     href: "https://www.chrisware.agency/",
+    github: "https://github.com/ChristianWare/chris_ware_agency_ii",
   },
   {
     src: Elite,
@@ -34,6 +36,7 @@ export const projects = [
     description:
       "Elite Retreat Rentals is a fictional website I built to show potential clients what a personalized booking website could look like. It allows users to view all available vacation rental properties listed by the owner, and they can make reservations on each property via the full-function booking system. Users can pay for each reservation in advance using Stripe. All are built in Next.js with Mongodb for the database and Next Auth for the authentication.",
     href: "https://case-study-one.vercel.app/",
+    github: "https://github.com/ChristianWare/Case-Study-One",
   },
   {
     src: Nier,
@@ -41,6 +44,7 @@ export const projects = [
     description:
       "Nier Transportation is a client who needed a new website that advertised his luxury black car driving service. This project is primarily front-end built with Next.js and has a markdown blog that utilizes the local file system with node.js. All styling was done with CSS Modules.",
     href: "https://www.niertransportation.com/",
+    github: "https://github.com/ChristianWare/Nier-Transport-Redesign",
   },
 ] as const;
 
