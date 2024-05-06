7255cd8ee44d639fb019bca1892948b7d31e8872
Chris Ware <c.p.ware@hotmail.com>
2024-05-06 00:16:41 -0700

    Scroll Fix

--- components/ScrollHorizontalText/ScrollHorizontalText.tsx
+++ components/ScrollHorizontalText/ScrollHorizontalText.tsx
@@ -11,50 +11,44 @@ interface Props {
 }
 
 const ScrollHorizontalText = ({ text }: Props) => {
-  
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
 
-  // const firstText = useRef(null);
-  // const secondText = useRef(null);
-  // const slider = useRef(null);
-  // let xPercent = 0;
-  // let direction = -1;
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.25,
+        start: 0,
+        end: window.innerHeight,
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-500px",
+    });
+    requestAnimationFrame(animate);
+  }, []);
 
-  // useEffect(() => {
-  //   gsap.registerPlugin(ScrollTrigger);
-  //   gsap.to(slider.current, {
-  //     scrollTrigger: {
-  //       trigger: document.documentElement,
-  //       scrub: 0.25,
-  //       start: 0,
-  //       end: window.innerHeight,
-  //       // eslint-disable-next-line react-hooks/exhaustive-deps
-  //       onUpdate: (e) => (direction = e.direction * -1),
-  //     },
-  //     x: "-500px",
-  //   });
-  //   requestAnimationFrame(animate);
-  // }, []);
-
-  // const animate = () => {
-  //   if (xPercent < -100) {
-  //     xPercent = 0;
-  //   } else if (xPercent > 0) {
-  //     xPercent = -100;
-  //   }
-  //   gsap.set(firstText.current, { xPercent: xPercent });
-  //   gsap.set(secondText.current, { xPercent: xPercent });
-  //   requestAnimationFrame(animate);
-  //   xPercent += 0.1 * direction;
-  // };
+  const animate = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    requestAnimationFrame(animate);
+    xPercent += 0.02 * direction;
+  };
 
   return (
     <section className={styles.sliderContainer}>
-      <div 
-      // ref={slider} 
-      className={styles.slider}>
-        <h2 
-        // ref={firstText}
-        >
+      <div ref={slider} className={styles.slider}>
+        <h2 ref={firstText}>
           {" "}
           {text}
           <span>
@@ -96,10 +90,248 @@ const ScrollHorizontalText = ({ text }: Props) => {
           <span>
             <Arrow className={styles.icon2} />
           </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
         </h2>
-        <h2 
-        // ref={secondText}
-        >
+        <h2 ref={secondText}>
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
+          {text}
+          <span>
+            <Arrow className={styles.icon2} />
+          </span>{" "}
           {text}
           <span>
             <Arrow className={styles.icon2} />
